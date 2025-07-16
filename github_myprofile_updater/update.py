c5143483bfdf4e2600545f450cf46bf78b08d249
zyy-2001 <3138089166@qq.com>
2025-07-17 07:47:44 +0800

    init

--- github_myprofile_updater/update.py
+++ github_myprofile_updater/update.py
@@ -1,5 +1,5 @@
 if __name__ == '__main__':
-    _header = "### Hi there, I'm [Siteng Huang (黄思腾 in Chinese)](https://kyonhuang.top/)! 👋 You can also call me Kyon Huang."
+    _header = "### Hi there, I'm [Yiyun Zhou (周逸云 in Chinese)](https://zyyz.fun/)! 👋 You can also call me smallz."
     base_dir = '../_pages/includes/'
     _intro = open(f'{base_dir}/intro.md').read().strip()
     # _homepage = open(f'{base_dir}/homepage.md').read().strip()
@@ -10,7 +10,7 @@ if __name__ == '__main__':
         # f.write('\n\n')
         f.write(_intro)
         f.write('\n\n')
-        f.write('Welcome to refer to my full publication list at [my personal homepage](https://kyonhuang.top/#-publications).')
+        f.write('Welcome to refer to my full publication list at [my personal homepage](https://zyyz.fun/#-publications).')
         f.write('\n')
         f.write('<p align="center">'+'<a href="https://twitter.com/KyonHuang" target="_blank"><img src="https://img.shields.io/twitter/follow/KyonHuang.svg?style=social" alt="Twitter"></a> '+'<a href="https://github.com/bighuang624?tab=followers" target="_blank"><img src="https://img.shields.io/github/followers/bighuang624.svg?label=Follow%20@bighuang624&style=social" alt="GitHub"></a> '+'<a href="https://github.com/bighuang624" target="_blank"><img src="https://img.shields.io/github/stars/bighuang624.svg?style=social" alt="GitHub"></a>'+'</p>')
         f.write('\n\n##')
